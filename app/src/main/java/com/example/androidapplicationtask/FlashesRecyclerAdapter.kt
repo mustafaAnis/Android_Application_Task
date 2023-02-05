eb61bb90a18b5a9d58eb34865f9d0e41d75fca97
Laptop <Mustafaanis247@gmail.com>
2023-02-05 21:09:57 +0500

    complete and done

--- app/src/main/java/com/example/androidapplicationtask/FlashesRecyclerAdapter.kt
+++ app/src/main/java/com/example/androidapplicationtask/FlashesRecyclerAdapter.kt
@@ -1,34 +1,41 @@
 package com.example.androidapplicationtask
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
 
-class FlashesRecyclerAdapter(val flashes:List<Post>, val context: Flashes): RecyclerView.Adapter<FlahsesViewHolder>() {
+class FlashesRecyclerAdapter(val flashes: List<Post>, val context: Context) :
+    RecyclerView.Adapter<FlashesRecyclerAdapter.FlahsesViewHolder>() {
     var url = "https://islam360.app/islam360/posts/image/urdu/"
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlahsesViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.flashes_list_view,parent,false)
+        val view = inflater.inflate(R.layout.flashes_list_view, parent, false)
         return FlahsesViewHolder(view)
     }
+
+    override fun onBindViewHolder(holder: FlahsesViewHolder, position: Int) {
+
+
+        Log.d("recycle", url + flashes[position].path)
+        Picasso.get().load(url + flashes[position].path).into(holder.imgView)
+
+    }
+
 
     override fun getItemCount(): Int {
         return flashes.size
     }
 
-    override fun onBindViewHolder(holder: FlahsesViewHolder, position: Int) {
 
-        Glide.with(context).load(url+flashes[position].path).into(holder.imgView)
+    class FlahsesViewHolder(flashesView: View) : RecyclerView.ViewHolder(flashesView) {
+        var imgView = flashesView.findViewById<ImageView>(R.id.flashesImageView)
 
     }
-
-
 }
 
-class FlahsesViewHolder(flashesView: View) :RecyclerView.ViewHolder(flashesView){
-var imgView = flashesView.findViewById<ImageView>(R.id.flashesView)
-
-}
